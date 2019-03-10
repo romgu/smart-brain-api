9e053b5a707c97d1e05e7d7cbb35b7c78078a254
romgu <gmarca20@gmail.com>
2019-03-10 19:44:25 -0300

    upgrading database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controller/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-encircled-98269',
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
